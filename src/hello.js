fcea8868bae787cbac30e06f0854998845fabc83
epstein.w <epstein.w@husky.neu.edu>
2018-07-17 00:24:22 -0400

    working through catchup

---
+++ src/hello.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class HelloWorld extends React.Component {
+    render() {
+        return (
+            <h1>Hello please</h1>
+        )
+    }
+}
+
+export default HelloWorld;
